5a29559aae7340f21d81e8d534298cdbe5d455d1
Nicolae <nicolaibarbarasa@gmail.com>
2024-05-27 14:27:03 +0200

    add php artisan db:seed --class=ComicsTableSeeder
    add php artisan make:model Comic -msrc
    
    add php artisan make:controller ComicController
    
    add Route

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic;
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    public function index()
+    {
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
+    }
+
+    public function show(Comic $comic)
+    {
+        return view('comics.show', compact('comic'));
+    }
+
+    public function create()
+    {
+        return view('comics.create');
+    }
+
+    public function store(Request $request)
+    {
+        Comic::create($request->all());
+        return redirect()->route('comics.index');
+    }
+
+    public function edit(Comic $comic)
+    {
+        return view('comics.edit', compact('comic'));
+    }
+
+    public function update(Request $request, Comic $comic)
+    {
+        $comic->update($request->all());
+        return redirect()->route('comics.index');
+    }
+
+    public function destroy(Comic $comic)
+    {
+        $comic->delete();
+        return redirect()->route('comics.index');
+    }
+}
