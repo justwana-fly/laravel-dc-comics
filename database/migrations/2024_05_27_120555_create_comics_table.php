c0c2a4bb5a0dfad8fd47de318b2bf23f3dde987e
Nicolae <nicolaibarbarasa@gmail.com>
2024-05-27 14:07:24 +0200

    add struttura della tabella

--- database/migrations/2024_05_27_120555_create_comics_table.php
+++ database/migrations/2024_05_27_120555_create_comics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateComicsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price', 20)->nullable();
+            $table->string('series', 100)->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type', 50)->nullable();
             $table->timestamps();
         });
     }
@@ -28,4 +35,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('comics');
     }
-};
+}
